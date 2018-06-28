baba826b928efe7b4a0cc48bdc35a5b3798091a2
Rai <PRRAI@BCGOV>
2018-06-28 14:32:35 -0700

    Adding the left navbar

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+import datetime
+import urllib.request
+import requests
+from xml.etree import ElementTree
+import sys
+
 
 
 # Create your views here.
+file_opml = 'C:\\Users\\PRRAI\\Desktop\\rssfeed.opml'
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    now = 'hello'
+    words = {'hello', 'hi', 'there'}
+
+    top_headings = []
+    with open(file_opml, 'rt') as f:
+        tree = ElementTree.parse(f)
+    for node in tree.findall('body')[0]:
+        top_heading = node.attrib.get('title')
+        top_headings.append(top_heading)
+
+    def find_sub_heading(index):
+        sub_headings = []
+        with open(file_opml, 'rt') as f:
+            tree = ElementTree.parse(f)
+            root = tree.getroot()
+        for node in root.findall('body')[0][index]:
+            sub_heading = node.attrib.get('title')
+            sub_headings.append(sub_heading)
+        return sub_headings
+
+
+    return render(request, 'jobs\home.html', {'words': top_headings})
+
+
 
 
