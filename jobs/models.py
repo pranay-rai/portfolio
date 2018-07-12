2fe9173effa9903d9638e3145b6a76387a0adbb7
Rai <PRRAI@BCGOV>
2018-07-12 15:52:17 -0700

    TLS Cipher Suites Scan complete

--- jobs/models.py
+++ jobs/models.py
@@ -9,6 +9,4 @@ class Job(models.Model):
 class URLForm(forms.Form):
     url = forms.CharField(label = 'Enter URL  ', max_length=100)
 
-class Entity(models.Model):
-    name = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
+
--- jobs/views.py
+++ jobs/views.py
@@ -3,26 +3,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import urllib.request
+import ssl, socket
 
 import requests
+import datetime
+import subprocess
 
 
 from jobs.models import URLForm
-import whois
 
+
+class Entity():
+    def __init__(self, name, status, validity, expiry, expiryDays, protocols, cipherSuites):
+        self.name = name
+        self.status = status
+        self.validity = validity
+        self.expiry = expiry
+        self.expiryDays = expiryDays
+        self.protocols = protocols
+        self.cipherSuites = cipherSuites
 
 
 def home(request):
-
-    url = ""
-    #if request.method == 'POST':
-    #    form = URLForm(request.POST)
-
-    #    if form.is_valid():
-    #        url = URLForm.cleaned_data['url']
-    #else:
     form = URLForm()
-
     return render(request, 'jobs\home.html', {'form':form})
 
 def results(request):
@@ -35,16 +38,108 @@ def results(request):
         else:
             dataForm = URLForm()
 
-    #status = whois.whois(url)
-    try:
-        r=requests.head(url)
-        status='Live'
-    except requests.ConnectionError:
-        status = 'Not Live'
+
+    def checkStatus(url):
+        status=''
+        try:
+            r = requests.get(url, verify=True, stream=False)
+            status = 'Live'
+        except requests.ConnectionError:
+            status = 'Not Live'
+        return status
+
+    def check_ssl(url):
+        validity = ''
+        try:
+            req = requests.get(url, verify=True, stream=False)
+            validity = 'valid'
+        except:
+            validity = 'Invalid'
+        return validity
+
+    def expiryDate(url):
+        date = ''
+        hostname = url.replace("https://","")
+        try:
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(socket.socket(), server_hostname=hostname)
+            s.connect((hostname, 443))
+            cert = s.getpeercert()
+            date=cert['notAfter']
+            date = datetime.datetime.strptime(date,"%b %d %X %Y %Z").strftime("%b %d %Y")
+        except:
+            date = 'Could Not Retrieve'
+        return date
+
+    def expiryDays(url):
+        days=''
+        hostname = url.replace("https://", "")
+        try:
+            days = (datetime.datetime.strptime(expiryDate(url), "%b %d %Y")-datetime.datetime.now()).days
+        except:
+            days = "Could not retrieve"
+        return days
+
+    def findSupportedProtocols(url):
+        protocols = []
+        hostname = url.replace("https://", "")
+        try:
+            output = subprocess.getoutput('pysslscan scan --scan=server.preferred_ciphers --ssl2 --ssl3 --tls10 --tls11 --tls12 ' + hostname)
+            for i in output.splitlines():
+                if i.__contains__('SSLv3'):
+                    if i.__contains__("Protocol"):
+                        continue
+                    else:
+                        protocols.append('SSL3 ')
+                if i.__contains__('TLSv10'):
+                    if i.__contains__("Protocol"):
+                        continue
+                    else:
+                        protocols.append('TLS1.0 ')
+                if i.__contains__('TLSv11'):
+                    if i.__contains__("Protocol"):
+                        continue
+                    else:
+                        protocols.append('TLS1.1 ')
+                if i.__contains__('TLSv12'):
+                    if i.__contains__("Protocol"):
+                        continue
+                    else:
+                        protocols.append('TLS1.2 ')
+        except:
+            protocols = 'Could Not Fetch'
+
+        return ''.join(protocols)
+
+    def findSupportedCipherSuites(url):
+        hostname = url.replace("https://", "")
+        result = []
+        try:
+            output = subprocess.getoutput('pysslscan scan --scan=server.ciphers --ssl2 --ssl3 --tls10 --tls11 --tls12 ' + hostname)
+            for i in output.splitlines():
+                if i.__contains__('Accepted'):
+                    words = i.split()
+                    word1 = words[1]
+                    word2 = words[4]
+                    word3 = words[6]
+                    result.append(word1[word1.index('m')+1:word1.index('m')+7] + " -- " + word2 + " -- " +  word3[4:word3.rfind('\\')-3])
+        except:
+            result = 'Could not fetch'
+
+        return result
+
+    entity = Entity('', '', '', '', '', '', '')
+
+    entity.name = url.replace("https://","")
+    entity.status = checkStatus(url)
+    entity.validity = check_ssl(url)
+    entity.expiry = expiryDate(url)
+    entity.expiryDays = expiryDays(url)
+    entity.protocols = findSupportedProtocols(url)
+    entity.cipherSuites = findSupportedCipherSuites(url)
 
 
 
-
-    return render(request, 'jobs\\results.html', {'word':url, 'status':status})
+    return render(request, 'jobs\\results.html', {'word':url, 'entity':entity})
 
 
